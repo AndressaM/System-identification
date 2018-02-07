b2d01f62e58a4996069fda1b69dce8a9f8cdf732
Andressa Martins <andressamartins@ic.ufal.br>
2018-02-06 22:28:08 -0300

    start file

---
+++ smith_method.m
@@ -0,0 +1,20 @@
+%% read file
+
+a = dlmread('conjunto1.txt');
+plot(a(:,2),a(:,1))
+
+%% 
+yy = smooth(a(:,2),a(:,1),'moving');
+figure;
+plot(a(:,2),yy);
+figure;
+fig = fit(a(:,2),a(:,1),'poly')
+plot(fig)
+
+%%
+
+max = a(196,2); % ver tamanho
+
+%% 
+
+
