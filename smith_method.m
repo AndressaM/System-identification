3314eac15b3c54be4a8c55f8d47e95d3402f6a8b
gregoryalbertt <gregoryalbertt@gmail.com>
2018-02-20 00:14:13 -0300

    e(t) udpated

--- smith_method.m
+++ smith_method.m
@@ -1,4 +1,7 @@
-a = dlmread('conjunto3.txt');
+%% read file
+syms s;
+
+a = dlmread('conjunto1.txt');
 t = a(:,2);
 y = a(:,1); %y(t)
 y1 = y;
@@ -7,7 +10,7 @@ y1 = y;
 %% 
 %yy2 = smooth(a(:,2),a(:,1),'sgolay');
 yy2 = sgolayfilt(y,2,51);
-figure; 
+%figure; 
 %plot(a(:,2),yy2);
 %hold on
 %plot(t,y);
@@ -32,8 +35,8 @@ intcpt = y_infl - slope*t_infl;                                 % Calculate Inte
 tngt = slope*t + intcpt;                                        % Calculate Tangent Line
 
 %%
-
-max = a(196,1); % ver tamanho
+[size,aux] = size(y);
+max = a(size,1); % ver tamanho
 % aqui é a(196,1) , por conta da matriz que y,t;
 %estavamos pegando o tempo antes
 min = a(1,1);
@@ -65,19 +68,39 @@ tau = t2 - t1;
 theta = t1;
 kp = max - min;
 
-Gzn = tf([kp],[tau,1],'OutputDelay',theta)
-[y,t_step] =step(Gzn,7);
+%Gzn = tf([kp],[tau,1],'OutputDelay',theta);
+%[y,t_step] =step(Gzn,7);
+
+Gzn = (kp/(tau*s+1))*exp(-theta*s);
+Gzn2 = Gzn/s;
+gt = ilaplace(Gzn2);
+ht = matlabFunction(gt);
+y_step = ht(t);
+plot(t,y_step)
 
 %% e(t)
-e = yy2-y;
-EQM = sum(e.^2)/lenght;
+e = yy2-y_step;
+e_sum = sum(e.^2);
+
+EQM = e_sum/size;
 IAE = sum(abs(e));
-ISE = sum(e.^2);
-%ITAE
+ISE = e_sum;
+ITAE = sum(t.*e);
+
+
+% e(t)
+e = yy2-y_step;
+e_sum = sum(e.^2);
+
+EQM = e_sum/size;
+IAE = sum(abs(e));
+ISE = e_sum;
+ITAE = sum(t.*e);
+
 
 %% Comparando com entrada ao degrau
 plot(y,t);
-
+    
 %% Hagglund
 
 kp = max - min;
@@ -90,10 +113,24 @@ theta = t1;
 t3 = input('Valor de t3 63,2%:');
 tau = t3-t1;
 
-GHAG = tf([kp],[tau,1],'OutputDelay',theta)
-[y,t_step] =step(GHAG);
+GHAG = (kp/(tau*s+1))*exp(-theta*s);
+GHAG2 = GHAG/s;
+gt = ilaplace(GHAG2);
+ht = matlabFunction(gt);
+y_step = ht(t);
+plot(t,y_step)
 
-%% Smith 1end Order
+% e(t)
+e = yy2-y_step;
+e_sum = sum(e.^2);
+
+EQM = e_sum/size;
+IAE = sum(abs(e));
+ISE = e_sum;
+ITAE = sum(t.*e);
+
+
+%% Smith 1st Order
 
 K =input('Valor de K: ');
 t1=input('Valor de t1: ');
@@ -105,7 +142,22 @@ if(theta<0)
     theta=0;
 end
 
-GS1 = tf([kp],[tau,1],'OutputDelay',theta);
+GS1 = (kp/(tau*s+1))*exp(-theta*s);
+GS12 = GS1/s;
+gt = ilaplace(GS12);
+ht = matlabFunction(gt);
+y_step = ht(t);
+plot(t,y_step)
+
+% e(t)
+e = yy2-y_step;
+e_sum = sum(e.^2);
+
+EQM = e_sum/size;
+IAE = sum(abs(e));
+ISE = e_sum;
+ITAE = sum(t.*e);
+
 
 
 %% Sundaresan e Krishmawany
@@ -117,7 +169,22 @@ t2=input('Valor de t2: ');
 tau = 0.67*(t2-t1);
 theta = 1.3*t1-0.29*t2;
 
-GSK = tf([kp],[tau,1],'OutputDelay',theta);
+GSK = (kp/(tau*s+1))*exp(-theta*s);
+GSK2 = GSK/s;
+gt = ilaplace(GSK2);
+ht = matlabFunction(gt);
+y_step = ht(t);
+plot(t,y_step)
+
+% e(t)
+e = yy2-y_step;
+e_sum = sum(e.^2);
+
+EQM = e_sum/size;
+IAE = sum(abs(e));
+ISE = e_sum;
+ITAE = sum(t.*e);
+
 
 
 %% PLot 
@@ -131,4 +198,4 @@ step(GSK,0.9);%SeK
 step(GM,0.9); %mollenkamp
 step(GS2,0.9);%Smith 2
 title('CONJUNTO 1');
-legend('Curval Original','Zigler e Nichols','Hägglund','Smith 1º ordem','Sudaresan e Krishnaswamy','MollenKamp','Smith 2º ordem')
+legend('Curval Original','Zigler e Nichols','Hägglund','Smith 1º ordem','Sudaresan e Krishnaswamy','MollenKamp','Smith 2º ordem');
