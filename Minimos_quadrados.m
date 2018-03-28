6fd6e088963fafdad014c3aa6d3bc96c676e5fb8
gregoryalbertt <gregoryalbertt@gmail.com>
2018-03-28 01:07:22 -0300

    2nd order process working on MMQ

--- Minimos_quadrados.m
+++ Minimos_quadrados.m
@@ -18,7 +18,8 @@ gs2 = tf(num2, den2);
 plot(t_gs1, y_gs1);
 hold on;
 plot(t_gs2, y_gs2);
-legend('gs1','gs2');
+legend('Sistema 1','Sistema 2');
+title('Comparação entre os sistemas');
 
 % Analise a resposta de ambos em função dos polos
 %
@@ -36,18 +37,25 @@ gz2 = c2d(gs2,0.1);
 figure;
 [y_gz1, n_gz1] = step(gz1,0:0.1:12);
 step(gs1,'k',gz1,'--')
+title('Sistema 1');
+legend('Contínuo','Discreto');
 figure;
 step(gs2,'r',gz2,'--')
+title('Sistema 2');
+legend('Contínuo','Discreto');
 
-%% Gerando valores simulados a partir da eq a diferenças para entrada ao degrau;
+%% Gerando valores simulados a partir da eq a diferenças para entradas diferentes;
 % Processo de 2a ordem
 
-u = ones(30);
+u = ones(100);
 Y = [];
 phi = [];
 
 y(1:2) = 0; %Definindo condição inicial do sistema
 u(1:2) = 0;
+
+y(3:100) = y_gz1(1:98);
+
 
 for i = 3:length(u)
     y1 = y(i-1);
@@ -69,19 +77,23 @@ a2 = theta(2);
 b1 = theta(3);
 b2 = theta(4);
 
-for t=3:length(u)
+for t=3:(length(y_gz1)+2)
     y_est(t) = -a1*y_est(t-1) -a2*y_est(t-2) + b1*u(t-1) + b2*u(t-2);
 end
 
-plot(y);
+y_est = y_est(3:end);
+
+
+figure;
+plot(y_gz1);
 hold on
 plot(y_est)
-
-%% Gerando valores simulados a partir da eq a diferenças para entrada definida;
-% Processo de 2a ordem
-
+title('Saída real x estimada');
+legend('y real', 'y estimado');
 
 
-
+%% Clear and Close
+clear
+clc
 
 
